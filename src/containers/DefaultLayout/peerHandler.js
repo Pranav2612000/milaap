900f8a911b0b734d3c51f87b501656f8dd4bc31f
Ved Dandekar <veddandekar6@gmail.com>
2020-05-14 01:59:46 +0530

    Implemented video calling

--- src/containers/DefaultLayout/peerHandler.js
+++ src/containers/DefaultLayout/peerHandler.js
@@ -1,12 +1,23 @@
 import React from "react";
-import {Button, Input, Row, Col} from 'reactstrap';
+import { Button, Input, Row, Col } from "reactstrap";
 import Peer from "peerjs";
 
 class PeerHandler extends React.Component {
   constructor() {
     super();
     this.state = {
-      selfPeer: new Peer(),
+      selfPeer: new Peer({
+        config: {
+          iceServers: [
+            { urls: "stun:stun.l.google.com:19302" },
+            {
+              url: "turn:numb.viagenie.ca",
+              credential: "HWeF3pu@u2RfeYD",
+              username: "veddandekar6@gmail.com",
+            },
+          ],
+        } /* Sample servers, please use appropriate ones */,
+      }),
       remotePeer: null,
       inText: null,
     };
@@ -33,12 +44,37 @@ class PeerHandler extends React.Component {
         },
         () => {
           call.answer();
+          self.state.call.on("error", (err) => console.log(err));
           call.on("stream", function (stream) {
-            self.videoRef.current.srcObject = stream;
+            // self.videoRef.current.srcObject = stream;
+            document.querySelector("video").srcObject = stream;
+            console.log(stream);
           });
         }
       );
     });
+  };
+
+  shareVideo = () => {
+    const self = this;
+    navigator.mediaDevices
+      .getUserMedia({
+        video: { width: 1024, height: 576 },
+        audio: true,
+      })
+      .then((media) => {
+        self.setState(
+          {
+            call: self.state.selfPeer.call(self.state.remotePeer.peer, media),
+          },
+          () => {
+            self.state.call.on("error", (err) => console.log(err));
+            self.state.call.on("stream", function (stream) {
+              self.videoRef.current.srcObject = stream;
+            });
+          }
+        );
+      });
   };
 
   shareScreen = () => {
@@ -54,7 +90,8 @@ class PeerHandler extends React.Component {
             call: self.state.selfPeer.call(self.state.remotePeer.peer, media),
           },
           () => {
-            this.state.call.on("stream", function (stream) {
+            self.state.call.on("error", (err) => console.log(err));
+            self.state.call.on("stream", function (stream) {
               self.videoRef.current.srcObject = stream;
             });
           }
@@ -64,11 +101,9 @@ class PeerHandler extends React.Component {
 
   connectToPeer = () => {
     const self = this;
-    this.setState(
-      {
-        remotePeer: this.state.selfPeer.connect(this.state.inText),
-      }
-    );
+    this.setState({
+      remotePeer: this.state.selfPeer.connect(this.state.inText),
+    });
   };
 
   inputHandler = (e) => {
@@ -84,17 +119,27 @@ class PeerHandler extends React.Component {
         <h3>{this.state.myID}</h3>
         {this.state.remotePeer ? (
           <div className="mt-4 text-center">
-            <Button className="m-4" color="success" onClick={this.shareScreen}>Share screen</Button>
-            <Button className="m-4" color="info" onClick={this.videoCall}>Share video</Button>
+            <Button className="m-4" color="success" onClick={this.shareScreen}>
+              Share screen
+            </Button>
+            <Button className="m-4" color="info" onClick={this.shareVideo}>
+              Share video
+            </Button>
           </div>
         ) : (
           <Row className="mt-4 text-center">
-          <Col xs={10}><Input onChange={this.inputHandler} /></Col>
-          <Col xs={2}><Button color="secondary" onClick={this.connectToPeer}>Connect</Button></Col>
+            <Col xs={10}>
+              <Input onChange={this.inputHandler} />
+            </Col>
+            <Col xs={2}>
+              <Button color="secondary" onClick={this.connectToPeer}>
+                Connect
+              </Button>
+            </Col>
           </Row>
         )}
         <br />
-        <video ref={this.videoRef} autoPlay></video>
+        <video autoPlay></video>
       </div>
     );
   }
