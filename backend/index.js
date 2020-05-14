e24f39717879bdf76e0148d6a8cd585b04d564d6
Pranav <pranav2000joglekar@gmail.com>
2020-05-14 12:51:53 +0530

    WIP: Adding RabbitMQ support.

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const connectDB = require('./config/db');
+//const rabbitDB = require('./config/rabbit');
 
 const app = express();
 
