81e07382cbc158550f6ff8d9aa820ab632e4a1f2
Pranav <pranav2000joglekar@gmail.com>
2020-05-14 12:19:44 +0530

    Implemented messaging using simple mongodb

--- src/views/Compose/index.js
+++ src/views/Compose/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Compose.css';
+import axios from 'axios';
 
 export default function Compose(props) {
+    const [msg, setMsg] = useState('');
+    async function sendMessage() {
+            const reqData = {
+                    sender: localStorage.getItem("uname"),
+                    msg: msg,
+                    roomName: props.roomName
+            };
+            axios.post('http://localhost:5000/api/room/sendmessage',
+                    reqData)
+                    .then(res => {
+                            console.log(res);
+                            window.location.reload();
+                    }).catch(err => {
+                            console.log(err);
+                    });
+    }
     return (
       <div className="compose">
         <input
           type="text"
           className="compose-input"
           placeholder="Type a message, @name"
+          value = {msg}
+          onChange = {e => setMsg(e.target.value)}
         />
+              <button className="compose-button primary" onClick={sendMessage}>Send</button>
 
         {
           props.rightItems
--- src/views/MessageList/index.js
+++ src/views/MessageList/index.js
@@ -175,24 +175,30 @@ export default function MessageList(props) {
     return(
       <div className="message-list">
         <Toolbar
-          title="Conversation Title"
+          title={props.roomName}
+                /*
           rightItems={[
             <ToolbarButton key="info" icon="ion-ios-information-circle-outline" />,
             <ToolbarButton key="video" icon="ion-ios-videocam" />,
             <ToolbarButton key="phone" icon="ion-ios-call" />
           ]}
+                 */
         />
 
         <div className="message-list-container">{renderMessages()}</div>
 
-        <Compose rightItems={[
+        <Compose 
+            rightItems={[
           <ToolbarButton key="photo" icon="ion-ios-camera" />,
           <ToolbarButton key="image" icon="ion-ios-image" />,
           <ToolbarButton key="audio" icon="ion-ios-mic" />,
           <ToolbarButton key="money" icon="ion-ios-card" />,
           <ToolbarButton key="games" icon="ion-logo-game-controller-b" />,
           <ToolbarButton key="emoji" icon="ion-ios-happy" />
-        ]}/>
+        ]}
+          roomName = {props.roomName}
+        />
+
       </div>
     );
 }
