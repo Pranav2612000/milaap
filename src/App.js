411c301b3fa2d3662182d0030bf4b1f8fd3159a0
Savan Nahar <savannahar68@gmail.com>
2020-05-31 16:00:28 +0530

    Redux setup done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 // import { renderRoutes } from 'react-router-config';
 import './App.scss';
 
+//Redux related imports
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 const loading = () => (
   <div className="animated fadeIn pt-3 text-center">Loading...</div>
 );
@@ -21,52 +25,54 @@ const Landing = React.lazy(() => import('./views/Pages/Landing/Landing'));
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <React.Suspense fallback={loading()}>
-          <Switch>
-            <Route
-              exact
-              path="/landing"
-              name="Landing Page"
-              render={(props) => <Landing {...props} />}
-            />
-            <Route
-              exact
-              path="/login"
-              name="Login Page"
-              render={(props) => <Login {...props} />}
-            />
-            <Route
-              path="/join"
-              name="Guest Login"
-              render={(props) => <Guest {...props} />}
-            />
-            <Route
-              exact
-              path="/register"
-              name="Register Page"
-              render={(props) => <Register {...props} />}
-            />
-            <Route
-              exact
-              path="/404"
-              name="Page 404"
-              render={(props) => <Page404 {...props} />}
-            />
-            <Route
-              exact
-              path="/500"
-              name="Page 500"
-              render={(props) => <Page500 {...props} />}
-            />
-            <Route
-              path="/"
-              name="Home"
-              render={(props) => <DefaultLayout {...props} />}
-            />
-          </Switch>
-        </React.Suspense>
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <React.Suspense fallback={loading()}>
+            <Switch>
+              <Route
+                exact
+                path="/landing"
+                name="Landing Page"
+                render={(props) => <Landing {...props} />}
+              />
+              <Route
+                exact
+                path="/login"
+                name="Login Page"
+                render={(props) => <Login {...props} />}
+              />
+              <Route
+                path="/join"
+                name="Guest Login"
+                render={(props) => <Guest {...props} />}
+              />
+              <Route
+                exact
+                path="/register"
+                name="Register Page"
+                render={(props) => <Register {...props} />}
+              />
+              <Route
+                exact
+                path="/404"
+                name="Page 404"
+                render={(props) => <Page404 {...props} />}
+              />
+              <Route
+                exact
+                path="/500"
+                name="Page 500"
+                render={(props) => <Page500 {...props} />}
+              />
+              <Route
+                path="/"
+                name="Home"
+                render={(props) => <DefaultLayout {...props} />}
+              />
+            </Switch>
+          </React.Suspense>
+        </HashRouter>
+      </Provider>
     );
   }
 }
