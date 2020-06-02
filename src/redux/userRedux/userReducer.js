4db8b7f5f92edc85259fe9f123b3730f6a6eef8c
Vasu Sharma <vasusharma2017@outlook.comwq>
2020-06-02 21:47:47 +0530

    Redux : SignUp Done

--- src/redux/userRedux/userReducer.js
+++ src/redux/userRedux/userReducer.js
@@ -1,30 +1,35 @@
-import { FETCH_USERS_FAILURE, FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS } from './userActionTypes';
+import {
+  FETCH_USERS_FAILURE,
+  FETCH_USERS_REQUEST,
+  FETCH_USERS_SUCCESS
+} from './userActionTypes';
 
 const initalState = {
-    loading: false,
-    users: [],
-    error: '',
-}
+  loading: false,
+  users: [],
+  error: ''
+};
 
 export const userReducer = (state = initalState, action) => {
-    switch (action.type) {
-        case FETCH_USERS_REQUEST:
-            return {
-                ...state,
-                loading: true
-            }
-        case FETCH_USERS_SUCCESS:
-            return {
-                loading: false,
-                users: action.payload,
-                error: ''
-            }
-        case FETCH_USERS_FAILURE:
-            return {
-                loading: false,
-                users: [],
-                error: action.payload
-            }
-        default: return state;
-    }
-}
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: ''
+      };
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
