9334a1064218469da021a0bdd3704ef78ffa3f9b
root <root@crio-sysdesign1.us-central1-a.c.crio-systemdesign.internal>
2020-06-23 15:21:00 +0000

    chore: update global config to use prod servers

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = global.config = {
-  backendURL: 'http://localhost:5000',
-  peerJSServerURL: 'localhost'
+  backendURL: 'http://34.71.81.212:5000',
+  peerJSServerURL: '34.71.81.212'
 };
