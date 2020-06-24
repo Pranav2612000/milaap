821bd655ac1458b586335ff910c4960856e8c6c1
Ved Dandekar <42548201+veddandekar@users.noreply.github.com>
2020-06-24 11:50:57 +0530

    fix: usernames displayed below videos (#96)

--- src/views/Connection/Connect.js
+++ src/views/Connection/Connect.js
@@ -70,8 +70,25 @@ export class Peer extends Emitter {
       console.log('connected');
     });
     this.peer.on('stream', (data) => {
+      const self = this;
       console.log('stream received');
-      createVideoElement(this, data, this.their_id, '');
+      var reqData = {
+        id: my_id,
+        type: 0,
+        roomName: this.room
+      };
+      axios
+        .post(`${global.config.backendURL}/api/room/goonlinesimple`, reqData, {
+          headers: {
+            'milaap-auth-token': localStorage.getItem('milaap-auth-token')
+          }
+        })
+        .then((res) => {
+          res.data.online.map((each) => {
+            if (each.id === self.their_id)
+              createVideoElement(self, data, self.their_id, each.username);
+          });
+        });
     });
     socket.on('signalling', (data, from_id) => {
       if (from_id != this.their_id) {
