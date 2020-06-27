1a70f86f379faa425e8a09b86bb39fd48a2125de
Pranav <pranav2000joglekar@gmail.com>
2020-06-27 21:35:50 +0530

    fix: limit bandwidth to improve call quality

--- src/views/Connection/Connect.js
+++ src/views/Connection/Connect.js
@@ -24,6 +24,13 @@ export class Peer extends Emitter {
     this.peer = new SimplePeer({
       initiator: initiator,
       stream: stream,
+      trickle: true,
+      sdpTransform: sdp => {
+        let newSDP = sdp;
+        newSDP = setMediaBitrate(newSDP, 'video', 233);
+        newSDP = setMediaBitrate(newSDP, 'audio', 80);
+        return newSDP;
+      },
       config: {
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
@@ -406,4 +413,44 @@ export async function addScreenShareStream(self) {
       val.peer.addStream(self.state.myScreenStreamObj);
     });
   });
+}
+
+
+
+function setMediaBitrate(sdp, media, bitrate) {
+  let lines = sdp.split('\n')
+  let line = -1
+  for (let i = 0; i < lines.length; i++) {
+        if (lines[i].indexOf('m=' + media) === 0) {
+                line = i
+                break
+              }
+      }
+  if (line === -1) {
+      // log('Could not find the m line for', media)
+    return sdp
+  }
+  // log('Found the m line for', media, 'at line', line)
+
+  // Pass the m line
+  line++
+
+  // Skip i and c lines
+  while (lines[line].indexOf('i=') === 0 || lines[line].indexOf('c=') === 0) {
+    line++
+  }
+
+  // If we're on a b line, replace it
+  if (lines[line].indexOf('b') === 0) {
+    // log('Replaced b line at line', line)
+    lines[line] = 'b=AS:' + bitrate
+    return lines.join('\n')
+  }
+
+  // Add a new b line
+  // log('Adding new b line before line', line)
+  let newLines = lines.slice(0, line)
+  newLines.push('b=AS:' + bitrate)
+  newLines = newLines.concat(lines.slice(line, lines.length))
+  return newLines.join('\n')
 }
