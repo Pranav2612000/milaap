217a3027e38fd88a4684dba9cf71cfe38d58ad0c
AnupNair08 <nair.anup09@gmail.com>
2020-07-20 11:47:26 +0530

    improvement: removed dependency for parsing urls

--- src/views/Message/index.js
+++ src/views/Message/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import moment from 'moment';
 import './Message.css';
-import parse from 'html-react-parser';
 
 export default function Message(props) {
   function urlify(text) {
-    const expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
-    const urlRegex = new RegExp(expression);
-    return text.replace(urlRegex, function (url) {
-      let hurl = url;
-      if (!url.includes('http://') && !url.includes('https://')) {
-        hurl = 'https://' + url;
-      }
-      const k =
-        '<a href="' +
-        hurl +
-        '" style="color: #000000" target="_blank" rel="noopener noreferrer">' +
-        url +
-        '</a>';
-      return k;
-    });
+    const regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})|[a-zA-Z0-9]+\.[^\s]{2,}/gi;
+    let currentIndex = 0;
+    let match;
+    const returnList = [];
+    while ((match = regex.exec(text))) {
+      returnList.push(text.substring(currentIndex, match.index));
+      const url = text.substring(match.index, match.index + match[0].length);
+      returnList.push(
+        <a
+          href={url.includes('http') ? url : `https://${url}`}
+          target="_blank"
+          style={{ color: '#000000' }}
+          rel="noopener noreferrer">
+          {url}
+        </a>
+      );
+      currentIndex = match.index + match[0].length;
+    }
+    returnList.push(text.substring(currentIndex, text.length));
+    return returnList;
   }
-
   const { data, isMine, startsSequence, endsSequence, showTimestamp } = props;
   const friendlyTimestamp = moment(data.timestamp).format('LLLL');
   return (
@@ -42,7 +45,7 @@ export default function Message(props) {
           <b className={['sendBy', `${isMine}`].join(' ')}>
             {!isMine && data.sender}
           </b>
-          <div>{parse(urlify(data.msg))}</div>
+          <div>{urlify(data.msg)}</div>
         </div>
       </div>
     </div>
